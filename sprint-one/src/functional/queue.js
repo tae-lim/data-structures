1d64d4e5cae5caef00eb05f09b9a934be197ae07
tae-lim <taelim92@gmail.com>
2018-08-01 15:41:43 -0700

    Implemented someInstance.enqueue function

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,13 +7,36 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+  //adds a string value to the back of the storage.
+
+  var count = 1;
+    storage[count] = value;
+    count ++;
   };
 
   someInstance.dequeue = function() {
+  //return and removes a string value at the front of the queue
   };
 
   someInstance.size = function() {
+  //return the number of items in the queue.
   };
 
   return someInstance;
 };
+
+//First in first out
+//Last in last out
+
+// var groceryLine = Queue();
+
+// console.log(groceryLine); ->
+// groceryLine = {
+//   enqueue: function
+//   dequeue: function
+//   size: function
+// }
+
+// groceryLine.enqueue('hi');
+
+//every time you call queue, you get a new instance (aka object).
