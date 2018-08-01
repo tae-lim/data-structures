37cba24cb24ca9f5161e39048a1895b877f51252
tae-lim <taelim92@gmail.com>
2018-08-01 15:42:25 -0700

    Added helpful comments for Functional Stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,15 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+  //add a string to the top of the stack
   };
 
   someInstance.pop = function() {
+  //pop and remove the string on top of the stack
   };
 
   someInstance.size = function() {
+  //return the number of items in the stack
   };
 
   return someInstance;
