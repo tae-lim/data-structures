a4b3aa847ea5e602674e2cd8c1a6d94bf338a0f2
tae-lim <taelim92@gmail.com>
2018-08-02 10:40:29 -0700

    Edited comments out

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -6,16 +6,9 @@ var Queue = function() {
   var count = 0;
   var front = 0;
   var dequeued;
-  // var storage = {
-  //   '1': _____, <-dequeue
-  //   '2': _____,
-  //   '3': _____  <-enqueue
-  // };
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
-  //adds a string value to the back of the storage.
     storage[count] = value;
     count ++;
   };
@@ -30,8 +23,6 @@ var Queue = function() {
   };
 
   someInstance.size = function() {
-  //return the number of items in the queue.
-  //return storage.length;
     if (count - front < 0) {
       return 0;
     } else {
@@ -41,22 +32,3 @@ var Queue = function() {
 
   return someInstance;
 };
-
-//test comment
-
-// First in first out
-// Last in last out
-
-// var groceryLine = Queue();
-
-// console.log(groceryLine); ->
-// groceryLine = {
-//   enqueue: function
-//   dequeue: function
-//   size: function
-// }
-
-// groceryLine.enqueue('hi');
-// groceryLine.dequeue(); - > ???
-
-//every time you call queue, you get a new instance (aka object).
