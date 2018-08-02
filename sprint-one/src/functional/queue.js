b2a1de0f84fd7e7a7954dd33c281a5e880af3a6e
tae-lim <taelim92@gmail.com>
2018-08-02 10:14:03 -0700

    Implemented someInstance.dequeue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,30 +3,63 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
+  var front = 1;
+  var dequeued;
+  // var storage = {
+  //   '1': _____, <-dequeue
+  //   '2': _____,
+  //   '3': _____  <-enqueue
+  // };
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
   //adds a string value to the back of the storage.
-
-  var count = 1;
     storage[count] = value;
     count ++;
   };
 
   someInstance.dequeue = function() {
-  //return and removes a string value at the front of the queue
+    dequeued = storage[front];
+    delete storage[front];
+    front++;
+    return dequeued;
+
+    // var result = '';
+    // var smallest = null;
+
+    // // {
+    // //   '1': a,
+    // //   '2': b
+    // // }
+
+    // for (var key in storage) {
+    //   // result += storage[key];
+    //   // console.log(smallest);
+    //   if (key < smallest || smallest === null) {
+    //     smallest = key;
+    //   }
+
+
+    //   // delete storage[key];
+    //   break;
+    // }
+    // result = storage[smallest];
+    // delete storage[smallest];
+    // return result;
   };
 
   someInstance.size = function() {
   //return the number of items in the queue.
+  //return storage.length;
   };
 
   return someInstance;
 };
 
-//First in first out
-//Last in last out
+// First in first out
+// Last in last out
 
 // var groceryLine = Queue();
 
@@ -38,5 +71,6 @@ var Queue = function() {
 // }
 
 // groceryLine.enqueue('hi');
+// groceryLine.dequeue(); - > ???
 
 //every time you call queue, you get a new instance (aka object).
