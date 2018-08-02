0c67580ad0221e814107e8865603bc497f8f22b8
tae-lim <taelim92@gmail.com>
2018-08-02 09:48:05 -0700

    Implemented someInstance.pop function for stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,19 +3,49 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 1;
+  var poppedValue;
 
+  // var storage = {
+  //   '1': _____,
+  //   '2': _____,
+  //   '3': _____  <-push <-pop
+  // };
   // Implement the methods below
   someInstance.push = function(value) {
-  //add a string to the top of the stack
+    storage[count] = value;
+    count++;
   };
 
   someInstance.pop = function() {
-  //pop and remove the string on top of the stack
-  };
-
+  count--;
+  poppedValue = storage[count];
+  delete storage[count];
+  return poppedValue;
+};
   someInstance.size = function() {
   //return the number of items in the stack
   };
 
   return someInstance;
 };
+
+// var pancake = Stack();
+
+// console.log(pancake); ->
+// pancake = {
+//   push: function
+//   pop: function
+//   size: function
+// }
+
+// count = 0
+// pancake.push('hi');
+// count = 1
+
+// count = 2
+
+// pancake.push('hey');
+// count = 3
+// pancake.pop(); - > ('hey');
+
