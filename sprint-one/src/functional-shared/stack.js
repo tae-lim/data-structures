c3472e32c018e0e538bd9aa89a4ba496df554085
tae-lim <taelim92@gmail.com>
2018-08-02 19:49:09 -0700

    Implemented working solution for function-shared-stack.js

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var instance = {};
+  instance.storage = {};
+  instance.count = 0;
+  instance.poppedValue = null;
+
+  _.extend(instance, stackMethods);
+
+  return instance;
 };
+
+// var Books = Stack();
+
 
 var stackMethods = {};
 
+stackMethods.push = function (value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
 
+stackMethods.pop = function () {
+  if (this.count > 0) {
+      this.count--;
+    }
+    this.poppedValue = this.storage[this.count];
+    delete this.storage[this.count];
+    return this.poppedValue;
+};
+
+stackMethods.size = function () {
+  return this.count;
+};
