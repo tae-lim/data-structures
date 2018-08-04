65a4ac7f6a09742c3c80f3bdd1622895bca40e70
tae-lim <taelim92@gmail.com>
2018-08-04 09:57:07 -0700

    Implemented tree.js treeMethods.addChild

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,14 +3,20 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
+//every node
+//{value: 'hello', children: []}
+
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
