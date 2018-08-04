8e51f8fbe7df3a0a9b5e2976f2153a848657c2ee
tae-lim <taelim92@gmail.com>
2018-08-04 14:37:54 -0700

    Implemented set.js setPrototype.add

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,17 +1,24 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.count = 0;
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
+//takes any string and adds it to the set
+//Sets should not use up any more space than necessary. Once a value is added to a set, adding it a second time should not increase the size of the set.
 setPrototype.add = function(item) {
+  this._storage[this.count] = item;
+  this.count++;
 };
 
+//takes any string and returns a boolean reflecting whether it can be found in a set
 setPrototype.contains = function(item) {
 };
 
+//takes any string and removes it from the set, if present
 setPrototype.remove = function(item) {
 };
 
