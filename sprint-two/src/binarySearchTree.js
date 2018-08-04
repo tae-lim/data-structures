5df7ae38c153dade7d7aeb165b01667a655a6a53
tae-lim <taelim92@gmail.com>
2018-08-04 14:57:35 -0700

    Initializing for binarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,23 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = {};
+  this.right ={};
 };
 
+//A .left property, a binary search tree (BST) where all values are lower than the current value.
+//A .right property, a BST where all values are higher than the current value.
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+//.insert() method, which accepts a value and places it in the tree in the correct position.
+BinarySearchTree.prototype.insert = function (value) {
+
+};
+
+//.contains() method, which accepts a value and returns a boolean reflecting whether or not the value is contained in the tree.
+BinarySearchTree.prototype.contains = function () {
+
+};
+
+//.depthFirstLog() method, which accepts a callback and executes it on every value contained in the tree.
+BinarySearchTree.prototype.depthFirstLog = function () {
+
+};
