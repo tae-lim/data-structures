b53fbb2940626954bdc6a459b3d5e7803099b959
tae-lim <taelim92@gmail.com>
2018-08-05 22:15:12 -0700

    Implemented accurate version of Set with unique values and no iteration

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,36 +1,22 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set.count = 0;
   set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
-//takes any string and adds it to the set
-//Sets should not use up any more space than necessary. Once a value is added to a set, adding it a second time should not increase the size of the set.
 setPrototype.add = function(item) {
-  this._storage[this.count] = item;
-  this.count++;
+  this._storage[item] = item;
 };
 
-//takes any string and returns a boolean reflecting whether it can be found in a set
 setPrototype.contains = function(item) {
-  for (var node in this._storage) {
-    if (this._storage[node] === item) {
-      return true;
-    }
-  }
-  return false;
+  return this._storage[item] === item;
 };
 
 //takes any string and removes it from the set, if present
 setPrototype.remove = function(item) {
-  for (var node in this._storage) {
-    if (this._storage[node] === item) {
-      delete this._storage[node];
-    }
-  }
+  delete this._storage[item];
 };
 
 /*
